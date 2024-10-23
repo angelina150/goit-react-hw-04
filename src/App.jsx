887b40a4b2d6de236033ea602b4f5bdf3c9a1833
Angelina Chigiryova <chigiryova31@gmail.com>
2024-10-23 21:06:44 +0300

    changed name task  components

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import { useEffect, useState } from "react";
 import getPhotosBySearchValue from "./services/api";
 import "./App.css";
-import SearchBar from "./Components/SearchBar/SearchBar";
-import ImageModal from "./Components/ImageModal/ImageModal";
-import ImageGallery from "./Components/ImageGallery/ImageGallery";
-import Loader from "./Components/Loader/Loader";
-import ErrorMessage from "./Components/ErrorMessage/ErrorMessage";
-import LoadMoreBtn from "./Components/LoadMoreBtn/LoadMoreBtn";
+import SearchBar from "./components/SearchBar/SearchBar";
+import ImageModal from "./components/ImageModal/ImageModal";
+import ImageGallery from "./components/ImageGallery/ImageGallery";
+import Loader from "./components/Loader/Loader";
+import ErrorMessage from "./components/ErrorMessage/ErrorMessage";
+import LoadMoreBtn from "./components/LoadMoreBtn/LoadMoreBtn";
 
 function App() {
   const [photos, setPhotos] = useState([]);
